313d3ebd5fd9d79319900dbe133f66491169e504
juddyhuddy <justinl732@nycstudents.net>
2026-05-08 08:54:11 -0400

    Create - import random.py

---
+++ - import random.py
@@ -0,0 +1,16 @@
+- import random
+class user:
+    def __init__(self, name, power, hp):
+        self.name = name
+        self.power = power
+        self.hp = hp
+class shooter(user)
+    def __init__(self, name, power, hp):
+        self.name = name
+        self.power = power
+        self.hp = hp
+
+        
+samus = shooter("Samus",150,50)
+bowser = tank("bowser",50,150)
+link = sword("link",100,100)
