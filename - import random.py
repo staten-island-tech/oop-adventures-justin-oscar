0e1ee685f34469889ab8fb6c0ceda78786858e99
juddyhuddy <justinl732@nycstudents.net>
2026-05-11 08:50:02 -0400

    Update - import random.py

--- - import random.py
+++ - import random.py
@@ -1,16 +1,27 @@
-- import random
+
 class user:
     def __init__(self, name, power, hp):
         self.name = name
         self.power = power
         self.hp = hp
-class shooter(user)
+class shooter(user):
     def __init__(self, name, power, hp):
-        self.name = name
-        self.power = power
-        self.hp = hp
-
+      super().__init__(name, power, hp)
         
+
+class tank(user):
+    def __init__(self, name, power, hp):
+      super().__init__(name, power, hp)
+     
+class sword(user):
+    def __init__(self, name, power, hp):
+      super().__init__(name, power, hp)       
+
+    def display_info(self):
+       return f"sword: {self.name}, power: {self.power}, hp: {self.hp}"   
+    
 samus = shooter("Samus",150,50)
 bowser = tank("bowser",50,150)
-link = sword("link",100,100)
+link = sword("link","100",100)
+
+print(sword.display_info())
