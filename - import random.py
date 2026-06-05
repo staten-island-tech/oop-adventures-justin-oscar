ac96fad5988eab869cc24d3ef82a2928afe493d6
juddyhuddy <justinl732@nycstudents.net>
2026-06-05 09:24:33 -0400

    Update - import random.py

--- - import random.py
+++ - import random.py
@@ -25,4 +25,48 @@ import json
 characters = open("./characters.json")
 data = json.load(characters)
 
+<<<<<<< Updated upstream
 player = (input("which player")).capitalize()
+=======
+player = (input("which player")).capitalize()
+print(data[player]['moves'])
+
+player_health = (data[player]['hp'])
+npc = (input("which npc")).capitalize()
+npc_health = (data[npc]['hp'])
+print(player_health)
+print("player health")
+
+
+
+while lives > 0 and npc_lives > 0:
+    if player_health < 0 or player_health == 0:
+        attack = input('What attack? ')
+        damage = (data[player]['moves'][attack])
+        npc_health -= damage
+        npc_attack = random.choice(list(data[npc]['moves'].keys()))
+   
+        npc_damage = data[npc]['moves'][npc_attack]
+        player_health -= npc_damage
+        print("NPC Health:") 
+        print(npc_health)
+        
+        print("npc: It's my turn now!!")
+        print(npc_attack)
+        print("Player Health:")
+        print(player_health)
+        
+        print("Npc: Take that!")
+        if player_health < 0:
+            lives -= 1
+            player_health = data[player]['hp']
+    if lives == 0:
+        print("game over")
+
+        if npc_health <0:
+            npc_lives-=1
+            npc_health = data[npc]['hp']
+    if npc_lives == 0:
+        print("victory")
+
+>>>>>>> Stashed changes
